96826daf14ad2476a5a67854b354d44f41abe986
Michael Sproul <micsproul@gmail.com>
2015-03-08 15:22:19 -0700

    Haskell: Insert, get, remove.

--- haskell/src/Benchmark.hs
+++ haskell/src/Benchmark.hs
@@ -13,11 +13,25 @@ insertAll w = foldl insertSingle T.empty w
     where
         insertSingle trie str = T.insert str (BS.length str) trie
 
+getAll :: Trie Int -> [ByteString] -> [Maybe Int]
+getAll trie keys = map (flip T.lookup trie) keys
+
+removeAll :: Trie Int -> [ByteString] -> Trie Int
+removeAll trie keys = foldl (flip T.delete) trie keys
+
 main = do
     -- Read the input file.
     filename <- getEnv "TEST_FILE"
     contents <- BS.readFile filename
     let testWords = BSC.words contents
 
+    let fullTrie = insertAll testWords
+
     -- Run benchmarks.
-    defaultMain [ bgroup "bytestring-trie" [ bench "insertAll" $ whnf insertAll testWords ] ]
+    defaultMain [
+        bgroup "bytestring-trie" [
+            bench "insert" $ whnf insertAll testWords,
+            bench "get" $ nf (getAll fullTrie) testWords,
+            bench "remove" $ whnf (removeAll fullTrie) testWords
+            ]
+        ]
